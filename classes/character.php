83d9bcc80fbdc313b8a337d03be6db20c5dc2a37
rshatalov <rshatalov@gmail.com>
2014-05-21 17:22:56 +0800

    bugs

--- classes/character.php
+++ classes/character.php
@@ -52,7 +52,7 @@ QUERY;
     }
 
     public function template() {
-        $t = <<<TEMPLATE
+$t = <<<TEMPLATE
 <div id="character">
                 <form method="post" action="/character.php">
 <div id="basic">
@@ -82,8 +82,8 @@ QUERY;
         
     <div class="rel block tall narrow"><div class="above narrow">'PUNTI</div><input type="text" pattern="[0-9]*" class=""></div>
         
-        <div class="tall narrow block"></div>
-                 <div class="tall narrow black block"></div>
+        <div class="rel block tall narrow"><div class="above narrow">MOD.</div></div>
+                 <div class="tall narrow black block">PF</div>
                 <input name="" class="tall narrow block" placeholder="TOT">
                 <input name="cur_hp" class="tall medium block" placeholder="ATTUALI" value="{$this->i['cur_hp']}" {$this->disabled}>
                  <div class="tall medium block black">'RIDUZIONE DANNO</div>
@@ -103,7 +103,7 @@ QUERY;
         
         <input type="text" pattern="[0-9]*" class="tall narrow block">
         <div class="tall narrow block"></div>
-                  <div class="tall narrow black block"></div>
+                  <div class="tall narrow black block">CA</div>
                  <div class="tall narrow block"></div>
                  <div class="tall narrow block"></div>
                  <div class="tall narrow block"></div>
@@ -154,32 +154,38 @@ QUERY;
     </div>
     <div style="clear:both"></div>
                 
-                  <div class="string tall">
+    <div class="string tall">
         <div class="tall narrow black block">
             <div class="first">CAR</div>
             <div class="second"></div>
         </div>
-        
         <input type="text" pattern="[0-9]*" class="tall narrow block">
         <div class="tall narrow block"></div>
+        <div class="tall narrow block black">INIZ.</div>
     </div>
     <div style="clear:both"></div>
-                  
 </div>
-                <div id="basic2">
+<div id="basic2">
    <div class="tall string">
-      
-       </div>                 
-   </div>
-                <div style="clear:both"></div>
+   </div>                 
+</div>
+<div style="clear:both"></div>
                 
-                 <div class="tall string">
-     
-       </div>                 
-   </div>
-                 <div style="clear:both"></div>
-                <input type="submit">
-   <div class="tall medium">Send</div></form></div>             
+<div id="">
+    <div class='string small'>
+        <div class='rel small half-medium block'><div class='above half-medium'>SALVEZZA</div><div class='black'>TEMPRA</div></div>
+        <div class='rel small narrow block'><div class='above narrow'>TOT</div><div class=''></div></div>
+        <div class='rel small narrow block'><div class='above narrow'>BASE</div><div class=''></div></div>
+        <div class='rel small narrow block'><div class='above narrow'>MAGIA</div><div class=''></div></div>
+        <div class='rel small narrow block'><div class='above narrow'>VARI</div><div class=''></div></div>
+        <div class='rel small narrow block'><div class='above narrow'>TEMP</div><div class=''></div></div>
+    </div>
+</div>
+<div style="clear:both"></div>
+
+
+   <div class="tall medium">Send</div></form>
+</div>             
 {$this->uid} {$this->name}
 
 TEMPLATE;
